6a73f321a5997ed28002dc366f259e4c5d9bab70
Angelo Bordin <angelombordin@gmail.com>
2024-06-02 20:12:01 -0300

    create server file  to init http server

---
+++ server.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/angelobordin/api-go/configs"
+	"github.com/angelobordin/api-go/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	err := configs.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Put("/{id}", handlers.Update)
+	r.Delete("/{id}", handlers.Delete)
+	r.Get("/", handlers.List)
+	r.Get("/{id}", handlers.Get)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetApiPort()), r)
+}
